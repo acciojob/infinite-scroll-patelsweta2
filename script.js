4ce937a6d34073756eeb603df8e81b93f0f7e421
patelsweta2 <80040744+patelsweta2@users.noreply.github.com>
2024-02-26 20:35:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here!
 
+const list = document.getElementById("infi-list");
+
+for(let i = 1;i<=10;i++){
+	const listItem = document.getElementById("li");
+	listItem.textContent = `Item ${i}`;
+	list.appendChild(listItem);
+}
+
+list.addEventListener("scroll", function() {
+	if(list.scrollHeight - list.scrollTop === list.clientHeight) {
+		addMoreItems();
+	}
+});
+
+function addMoreItems() {
+	for(let i = 1;i<=2;i++){
+		const newItem = document.createElement("li");
+		newItem.textContent = `Item ${list.children.length + i}`;
+		list.appendChild(newItem);
+	}
+}
+
